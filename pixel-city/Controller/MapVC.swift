9dc70a0df61bb96810d16f5752fbf11f70d341c0
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-08-15 21:58:01 -0400

    init

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  pixel-city
 //
 //  Created by Richard Cummings on 2018-08-15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
