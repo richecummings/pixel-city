c676b50532697796643deb0ac16ef8c1021bae30
Richard Cummings <richardcummings@Richards-MacBook-Pro.local>
2018-08-19 16:35:02 -0400

    adding UICollectionView and PhotoCell class

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Richard Cummings on 2018-08-19.
+//  Copyright © 2018 Richard Cummings. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
